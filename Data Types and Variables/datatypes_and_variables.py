6eb565499ca1cefbb8d7e297234613429914b954
Michael Fodor <mfodor97@gmail.com>
2020-08-10 19:35:19 -0700

    Add files via upload

---
+++ Data Types and Variables/datatypes_and_variables.py
@@ -0,0 +1,55 @@
+### Data types ###
+# string (a STRING of characters)
+# int (an integer)
+# float (floating point)
+# bool (also called boolean, but just True or False)
+
+### Naming Convention ###
+# Camel case and underscore
+# The code below is mixed with both! Dont do this! I am just showing you both conventions
+
+
+### DATA TYPES ###
+# examples of integers
+spamCount = 4
+eggsSize = 2
+number_of_spam = 8
+amount_of_friends = 0
+
+# examples of float
+sizeOfPi = 3.14
+timeLeft = 1.1624
+y_coordinate = 84.23
+cellSize = 1.32
+
+# examples of string
+name_of_organelle = 'endoplamic reticulum'
+DNA_protein = 'T'
+DNA_protein2 = 'C'
+michaelsFavoritePizza = 'i like pepperoni on Tuesdays, but combo pizza on saturdays'
+userName = 'coolguy12'
+userPassword = '123abc!'
+
+# examples of boolean
+isDone = True
+isFinished = False
+isMonomer = False
+is_o_chem_fun = False
+
+### Mulitple Assignment ###
+x, y, z = 1, 2, 3
+fruit1, fruit2, fruit3 = 'apples', 'bananas', 'oranges'
+dadsFavoritePizza, momsFavoritePizza, brothersFavoritePizza = 'pepperoni', 'salami', 'hawaiian'
+
+### PRINT FUNCTION ###
+# print(variable goes here)
+
+print(True)
+print(isFinished)
+print(1)
+print(fruit2)
+print(michaelsFavoritePizza)
+print(userPassword)
+print(DNA_protein)
+
+
