006fa100c431f5e28e6fec3b7446ba9f08fdfb47
Michael Fodor <mfodor97@gmail.com>
2020-08-14 20:40:30 -0700

    Add files via upload

---
+++ Data Types and Variables/assignment3.py
@@ -0,0 +1,66 @@
+### PART 1 ###
+# What would the following print? Uncomment to see your answer
+
+# print("101 % 1=", 101%1)
+# print("101 % 10=", 101%10)
+# print("101 % 100=", 101%100)
+# print("101 % 1000=", 101%1000)
+
+a = 5
+a %= 7
+#print(a)
+
+b = 12
+b %= a
+#print(b)
+
+c = 5
+c %= b + 1
+#print(c)
+
+
+### PART 2 ###
+# Define a list named "Animal Cell"
+# Initialize it with the data: "Mito", "Golgi", "Ribos", "Nucleus", "E.R.", and "Lysos"
+# REMEMBER you use [brackets] to define a list
+
+# Create an variable named "i" and initialize it to 0 (i in this case is short for "index" which tracks the index
+#   of each value in your list)
+
+
+# Print every element of "Animal Cell" to the console using the "i" variable (you will need to use + )
+
+# Challenge: Without creating a new list, how can you append a new value to the back of your list in Python3?
+# Append "Perox" to your already existing list
+
+
+### PART 3 ###
+# Define a list named "Sugar List" and initialize it with the values "Dex", "Fruct", "Gluc", "Lact", "Sucr" and "Malt"
+
+# Define a variable named "My Fav Ose" and assign it the value of your favorite choice using the [] operator
+
+# Find the length of the Sugar List and assign it to a variable.
+
+# Challenge: Using the variable you just make (of list size), print to the console the last element in Sugar List
+#   without creating a new variable!
+
+
+### PART 4 ###
+# Define a variable named "User Name" and initialize it to: "Bob"
+# Define a variable named "Password" and initialize it to: "123abc"
+# Define a variable named "Zip" and initialize it to: 12345 <-- an int, not a string!
+# Define a variable named "In College" and initialize it to: False
+# Define a variable named "Coolness Level" and initialize it to: 3.6
+
+# Challenege: Define a variable named "Account List" and initialize it to contain the values using only the variables you
+#   defined from above
+
+# Find the length of the list and store it in a variable
+
+# Print to the console the entire list using only one variable name
+
+# Access the last element of the list using the [] operator and your length variable, and change its value to 4.6 but
+#   not create a new variable
+
+# Print to the console the entire list using only one variable name (Ensure that the last element is now 4.6)
+
