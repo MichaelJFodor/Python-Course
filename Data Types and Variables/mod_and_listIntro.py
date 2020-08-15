1b4af5a2e03cfbe2c7a1fb607a16fdc228a17771
Michael Fodor <mfodor97@gmail.com>
2020-08-14 20:28:25 -0700

    Add files via upload

---
+++ Data Types and Variables/mod_and_listIntro.py
@@ -0,0 +1,74 @@
+# MODULUS OPERATOR
+# Mod is the REMAINDER of division!
+
+print(' 0%5=', 0%5) # 0/5 = 0, remainder 0
+print(' 1%5=', 1%5) # 1/5 = 0, remainder 1
+print(' 2%5=', 2%5) # 2/5 = 0, remainder 2
+print(' 3%5=', 3%5) # 3/5 = 0, remainder 3
+print(' 4%5=', 4%5) # 4/5 = 0, remainder 4
+print(' 5%5=', 5%5) # 5/5 = 1, remainder 0
+print(' 6%5=', 6%5) # 6/5 = 1, remainder 1
+print(' 7%5=', 7%5) # 7/5 = 1, remainder 2
+print(' 8%5=', 8%5) # 8/5 = 1, remainder 3
+print(' 9%5=', 9%5) # 9/5 = 1, remainder 4
+print('10%5=',10%5) # 10/5= 2, remainder 0
+# ...
+print('11%5=',11%5)
+print('12%5=',12%5)
+print('13%5=',13%5)
+print('14%5=',14%5)
+print('15%5=',15%5)
+
+print(' 0%7=', 0%7)
+print(' 1%7=', 1%7)
+print(' 2%7=', 2%7)
+print(' 3%7=', 3%7)
+print(' 4%7=', 4%7)
+print(' 5%7=', 5%7)
+print(' 6%7=', 6%7)
+print(' 7%7=', 7%7)
+print(' 8%7=', 8%7)
+print(' 9%7=', 9%7)
+print('10%7=',10%7)
+print('11%7=',11%7)
+print('12%7=',12%7)
+print('13%7=',13%7)
+print('14%7=',14%7)
+print('15%7=',15%7)
+
+
+
+# LISTS
+
+'''
+Writing this...
+protein1 = 'G'
+protein2 = 'A'
+protein3 = 'T'
+protein4 = 'T'
+protein5 = 'A'
+protein6 = 'C'
+protein7 = 'A'
+Can be annoying..
+
+So we introduce a new type! its a list.
+A list is a "container" that holds a group of data
+The list can be accessed at any INDEX(place in the list) and can grow and shrink as desired
+
+Remember we start counting at 0
+So the first element in a list is: list[0]
+And the last element in a list is: list[listSize - 1]
+
+ex) where: proteinSize = len(protein)
+protein = ['G', 'A', 'T', 'T', 'A', 'C', 'A']
+
+
+protein[0] is 'G'
+protein[1] is 'A'
+...
+protein[proteinSize - 1]
+'''
+
+
+
+
